5ac4796b2faaaf97cc050722ed99dc47894adc95
Kunalpatil009 <009kunalpatil@gmail.com>
2022-08-06 15:36:08 +0530

    max days & hours condition added

--- com/bridgelabs/employee_wages/EmployeeWages.java
+++ com/bridgelabs/employee_wages/EmployeeWages.java
@@ -6,30 +6,33 @@ public class EmployeeWages {
     static int FULLDAYHOURS = 8;
     static int PARTIMEHOURS = 4;
     static int TOTALWAGES;
-    static int WORKINHOURS;
+    static int WORKINGHOURS;
     static int WORKINGDAYSPERMONTH = 20;
+    static int MAXWORKINGDAYS = 20;
+    static int MAXWORKINGHOURS = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wages Programme");
         int Absent = 0;
         int Full_time = 1;
         int Part_time = 2;
-        int working_hour;
-        for (int day = 1; day <= WORKINGDAYSPERMONTH; day++) {
+        int total_working_hours = 0;
+        System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Working hours", "Wage", "Total working hrs");
+        for (int day = 1; day <= MAXWORKINGDAYS && WORKINGHOURS <= MAXWORKINGHOURS; day++) {
             double empCheck = Math.floor(Math.random() * 10) % 3;
-            System.out.println(empCheck);
+//            System.out.println(empCheck);
             if (empCheck == Full_time) {
-                WORKINHOURS = 8;
-                System.out.println("Employee is present for full-time");
-                TOTALWAGES = WAGESPERHOUR * WORKINHOURS;
-                System.out.println("Wages :" + TOTALWAGES);
+                WORKINGHOURS = 8;
+//                System.out.println("Employee is present for full-time");
+                TOTALWAGES = WAGESPERHOUR * WORKINGHOURS;
+//                System.out.println("Wages :" + TOTALWAGES);
             } else if (empCheck == Part_time) {
-                WORKINHOURS = 4;
-                System.out.println("Employee is present for part-time");
-                TOTALWAGES = WAGESPERHOUR * WORKINHOURS;
-                System.out.println("Wages :" + TOTALWAGES);
+                WORKINGHOURS = 4;
+//                System.out.println("Employee is present for part-time");
+                TOTALWAGES = WAGESPERHOUR * WORKINGHOURS;
+//                System.out.println("Wages :" + TOTALWAGES);
             } else {
-                System.out.println("Employee is absent");
+//                System.out.println("Employee is absent");
 
 //        switch (empCheck) {
 //            case Full_time -> {
@@ -44,9 +47,11 @@ public class EmployeeWages {
 //            }
 //            default -> System.out.println("Employee is absent");
 //        }
-                int wages = WAGESPERHOUR * WORKINHOURS;
-                System.out.println("Day " + day + " wage is:" + wages);
+                int wages = WAGESPERHOUR * WORKINGHOURS;
+//                System.out.println("Day " + day + " wage is:" + wages + " Working Hours : " +total_working_hours);
+                System.out.printf("%5s     %5s     %5s    \n", day, WORKINGHOURS, wages );
                 TOTALWAGES += wages;
+
             }
         }
     }
